8e3861a0cc04ed7d98374f81faeccdde71b7eb5d
Drabs171 <143757933+Drabs171@users.noreply.github.com>
2025-09-10 12:46:50 -0700

    Add files via upload

---
+++ Date.java
@@ -0,0 +1,28 @@
+public class Date {
+    public static void main(String[] args) {
+        
+        String day = "Friday";
+        int day1 = 9;
+        String month = "September";
+        int year = 2025;
+        
+        
+        
+        System.out.print("The current date is ");
+        System.out.print(day);
+        System.out.print(", ");
+        System.out.print(month);
+        System.out.print(" ");
+        System.out.print(day1);
+        System.out.print(", ");
+        System.out.println(year);
+        System.out.print("European Format : ");
+        System.out.print(day);
+        System.out.print(" ");
+        System.out.print(day1);
+        System.out.print(" ");
+        System.out.print(month);
+        System.out.print(" ");
+        System.out.print(year);
+    }
+}
